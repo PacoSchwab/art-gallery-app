1765d6649f4bef333fa67f9d7b6cfe096efc2b0c
Ella Kempel <ella.lernt@mail.de>
2023-03-22 18:35:24 +0100

    add link to image

--- components/ArtPiecePreview/ArtPiecePreview.js
+++ components/ArtPiecePreview/ArtPiecePreview.js
@@ -4,15 +4,18 @@ export default function ArtPiecePreview({ src, name, artist, slug }) {
   console.log(slug);
   return (
     <li>
-      <Image
-        src={src}
-        alt={name}
-        width={300}
-        height={200}
-        style={{ borderRadius: "12px", objectFit: "cover" }}
-      />
-      <Link href={`/art-pieces/${slug}`}>{name}</Link>
-      {<p>{`"${name}" by ${artist}`}</p>}
+      <Link href={`/art-pieces/${slug}`}>
+        <Image
+          src={src}
+          alt={name}
+          width={300}
+          height={200}
+          style={{ borderRadius: "12px", objectFit: "cover" }}
+        />
+      </Link>
+      <br />
+      <Link href={`/art-pieces/${slug}`}>{`"${name}"`}</Link>
+      <span>{` by ${artist}`}</span>
     </li>
   );
 }
